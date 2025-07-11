5b3c4d76f43ad84b34addc8ad69f1ba02ab99420
Immanuel N <immanuelnjogu@students.tukenya.ac.ke>
2025-07-11 13:26:11 +0300

    create a decorator that logs database queries executed by any function

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,33 +1,28 @@
-import mysql.connector
+from datetime import datetime
+import sqlite3
 import functools
 
-# Decorator to log SQL queries
-def log_queries():
-    def decorator(func):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            query = kwargs.get('query') or (args[0] if args else None)
-            if query:
-                print(f"[SQL LOG] Executing query: {query}")
-            else:
-                print("[SQL LOG] No SQL query found in arguments.")
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
+#### decorator to log SQL queries
 
-@log_queries()
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        query = kwargs.get('query') or (args[0] if args else None)
+        if query:
+            print(f"[{datetime.now()}] [SQL LOG] Executing query: {query}")
+        else:
+            print(f"[{datetime.now()}] [SQL LOG] No SQL query found in arguments.")
+        return func(*args, **kwargs)
+    return wrapper
+
+@log_queries
 def fetch_all_users(query):
-    conn = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        password='Immamanu1234!',
-        database='ALX_prodev'
-    )
+    conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     cursor.execute(query)
     results = cursor.fetchall()
     conn.close()
     return results
 
-# Fetch users while logging the query
+#### fetch users while logging the query
 users = fetch_all_users(query="SELECT * FROM users")
