59ede450b20acbaa5270fc32d177114514a2b2ff
Immanuel N <immanuelnjogu@students.tukenya.ac.ke>
2025-07-20 22:47:53 +0300

    project set up

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -83,7 +83,11 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
     ]
 }
 # Password validation
