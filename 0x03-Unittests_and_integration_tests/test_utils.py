5c7fbf43959adada81b9a7323eb67a4d868fd03c
Immanuel N <immanuelnjogu@students.tukenya.ac.ke>
2025-07-20 20:13:06 +0300

    changes

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """Unit tests for the access_nested_map function in the utils module."""
-
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import unittest
 from parameterized import parameterized
 from utils import access_nested_map
