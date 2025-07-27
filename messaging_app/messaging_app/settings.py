ff65ebabe0cf112b6ecd1be010aa400f6bcb3da8
Immanuel N <immanuelnjogu@students.tukenya.ac.ke>
2025-07-27 21:03:05 +0300

    implement pagination and filtering for messages

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -94,7 +94,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
          'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'messaging_app.chats.pagination.MessagePagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 20,
     'DEFAULT_FILTER_BACKENDS': (
         'django_filters.rest_framework.DjangoFilterBackend',
