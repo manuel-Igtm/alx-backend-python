abe047b325b7455dbbba724d1647d16a53d62a5f
Immanuel N <immanuelnjogu@students.tukenya.ac.ke>
2025-07-27 23:15:48 +0300

    Create a middleware that logs each user’s requests to a file, including the timestamp, user and the request path.

--- Django-Middleware-0x03/messaging_app/settings.py
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -51,8 +51,11 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'chats.middleware.RequestLoggingMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+   
+
 ]
 
 ROOT_URLCONF = 'messaging_app.urls'
