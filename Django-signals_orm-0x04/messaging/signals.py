476a3a3330300dcb4cb9026542357f502746be49
Immanuel N <immanuelnjogu@students.tukenya.ac.ke>
2025-08-03 21:14:10 +0300

    Automatically clean up related data when a user deletes their account.

--- Django-signals_orm-0x04/messaging/signals.py
+++ Django-signals_orm-0x04/messaging/signals.py
@@ -1,6 +1,6 @@
-from django.db.models.signals import post_save, pre_save
+from django.db.models.signals import post_save, pre_save ,post_delete
 from django.dispatch import receiver
-from .models import Message, Notification,MessageHistory
+from .models import Message, Notification,MessageHistory,User
 
 @receiver(post_save, sender=Message)
 def create_notification_on_message(sender, instance, created, **kwargs):
@@ -21,3 +21,9 @@ def save_message_history(sender, instance, **kwargs):
                 previous_content=old_message.content
             )
             instance.edited = True
+
+@receiver(post_delete, sender=User)
+def cleanup_user_data(sender, instance, **kwargs):
+    # Related messages and notifications are deleted via CASCADE
+    # MessageHistory will be deleted through related messages
+    print(f"Cleaned up data for user {instance.username}")
